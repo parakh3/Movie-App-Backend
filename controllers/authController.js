66a6b0e08f4cbd19ff790a671ccc6485b06f0d55
parakh3 <parakhagarwal945@gmail.com>
2024-08-01 17:55:48 +0530

    updated signup controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,7 +8,7 @@ const generateToken = (id) => {
 
 
 const registerUser = async (req, res) => {
-  const { email, password } = req.body;
+  const { name,email, password } = req.body;
   const userExists = await User.findOne({ email });
 
   if (userExists) {
@@ -17,14 +17,16 @@ const registerUser = async (req, res) => {
   });
   }
 
-  const user = await User.create({ email, password });
-  console.log("checking user is",user);
+  const user = await User.create({ name,email, password });
+  
 
   if (user) {
     res.status(201).json({
       _id: user._id,
+      name:user.name,
       email: user.email,
       token: generateToken(user._id),
+      code: 200,
     });
   } else {
      res.status(400).json({
