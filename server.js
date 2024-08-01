e0a2bab5960d328af641a4f1189b42b7d95928ec
parakh3 <parakhagarwal945@gmail.com>
2024-08-01 19:27:35 +0530

    cors resolved

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000', // Allow requests from this origin
+  origin: '*', // Allow requests from this origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'] // Allow these headers
 }));
